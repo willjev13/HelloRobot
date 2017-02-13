bb99b2f40d6157c7380483f8405e2dd507a213c2
willjev13 <jevnikarw@gilmour.org>
2017-02-13 11:31:11 -0500

    dance1

--- hello.py
+++ hello.py
@@ -1,4 +1,11 @@
 from gopigo import *
+import time
 
-enc_tgt(1,1,72)
-fwd()
+def shimmy():
+    for x in range(2):
+        right()
+        time.sleep(.5)
+        left()
+        time.sleep(.5)
+shimmy()
+stop()
